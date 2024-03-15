8c504a2ce83c75436c916ccf86ee02c3b80b6989
Ghostfinder2253 <rodrigomansergas@gmail.com>
2024-03-15 19:57:42 +0100

    MagmaticOre

--- src/main/java/net/mcreator/mimod/MiModMod.java
+++ src/main/java/net/mcreator/mimod/MiModMod.java
@@ -31,7 +31,6 @@ import net.minecraft.network.FriendlyByteBuf;
 
 import net.mcreator.mimod.init.MiModModTabs;
 import net.mcreator.mimod.init.MiModModItems;
-import net.mcreator.mimod.init.MiModModBlocks;
 
 import java.util.function.Supplier;
 import java.util.function.Function;
@@ -50,8 +49,6 @@ public class MiModMod {
 	public MiModMod() {
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		MiModModBlocks.REGISTRY.register(bus);
 
 		MiModModItems.REGISTRY.register(bus);
 
